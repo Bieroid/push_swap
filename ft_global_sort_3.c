677b012dc10cff3a1d7ccde78a72e367bd5e4d24
Bieroid <83780932+Bieroid@users.noreply.github.com>
2021-10-06 21:18:58 +0300

    Add files via upload

--- ft_global_sort_3.c
+++ ft_global_sort_3.c
@@ -1,46 +1,28 @@
 #include "push_swap.h"
 
-void	ft_red_move_from_b(linked_list *a, linked_list *b, int *k)
+void	ft_red_move_from_b(linked_list *a, linked_list *b)
 {
 	if (b->head->order <= a->mid)
-	{
-		if (*k == 1)
-		{
-			ft_rr(b);
-			*k = 0;
-		}
-		else
-			ft_rb(b, 0);
-	}
+		ft_rb(b, 0);
 	else
 	{
 		b->head->flag = a->flag;
-		if (*k == 1)
-		{
-			ft_putstr("ra\n");
-			*k = 0;
-		}
 		ft_pa(a, b);
 	}
 }
 
-void	ft_red_b_next(linked_list *a, linked_list *b, int *k)
+void	ft_red_b_next(linked_list *a, linked_list *b)
 {
 	b->head->flag = a->flag;
 	ft_pa(a, b);
-	if (*k == 1)
-		ft_putstr("ra\n");
-	ft_ra(a, 1);
-	*k = 1;
+	ft_ra(a, 0);
 	a->next++;
 }
 
 void	ft_move_from_b(linked_list *a, linked_list *b)
 {
 	int	i;
-	int	k;
-
-	k = 0;
+	
 	while (ft_lstsize(b) != 0)
 	{
 		i = ft_lstsize(b) + 1;
@@ -50,9 +32,9 @@ void	ft_move_from_b(linked_list *a, linked_list *b)
 		while (--i > 0)
 		{
 			if (b->head->order == a->next)
-				ft_red_b_next(a, b, &k);
+				ft_red_b_next(a, b);
 			else
-				ft_red_move_from_b(a, b, &k);
+				ft_red_move_from_b(a, b);
 		}
 	}
 }
